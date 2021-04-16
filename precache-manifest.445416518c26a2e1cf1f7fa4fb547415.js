1cc734172598ed4d92df298918e4fe4eb2ee7398
qogadmin <admin@quantoptiongroup.com>
2021-04-16 04:43:38 +0000

    Updates

---
+++ precache-manifest.445416518c26a2e1cf1f7fa4fb547415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e3c297ce1499f08ee6ce6ef7968d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c2871546ded49f9c5e",
+    "url": "/static/css/2.b6642dd1.chunk.css"
+  },
+  {
+    "revision": "bb372f1738bc6c099670",
+    "url": "/static/css/main.fbde794f.chunk.css"
+  },
+  {
+    "revision": "09c2871546ded49f9c5e",
+    "url": "/static/js/2.18587033.chunk.js"
+  },
+  {
+    "revision": "68bd64590edaddbc2c76924e3401a365",
+    "url": "/static/js/2.18587033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb372f1738bc6c099670",
+    "url": "/static/js/main.e47f093c.chunk.js"
+  },
+  {
+    "revision": "d991981448ea8f9aa859",
+    "url": "/static/js/runtime-main.cbe038c4.js"
+  }
+]);
