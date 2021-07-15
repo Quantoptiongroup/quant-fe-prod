c0f47ac8910e645e1495e8f157f85f95c4662075
qogadmin <admin@quantoptiongroup.com>
2021-07-15 07:23:27 +0000

    Updates

---
+++ precache-manifest.fa58e0430b46885aed82fb33c2959b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed41237288db21526ffddc438ebb239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e0ac03fbc49538e42b",
+    "url": "/static/css/2.b6642dd1.chunk.css"
+  },
+  {
+    "revision": "e3790be237f46a94f8cb",
+    "url": "/static/css/main.b8d6d1fd.chunk.css"
+  },
+  {
+    "revision": "15e0ac03fbc49538e42b",
+    "url": "/static/js/2.d3e39748.chunk.js"
+  },
+  {
+    "revision": "68bd64590edaddbc2c76924e3401a365",
+    "url": "/static/js/2.d3e39748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3790be237f46a94f8cb",
+    "url": "/static/js/main.c6a662e9.chunk.js"
+  },
+  {
+    "revision": "d991981448ea8f9aa859",
+    "url": "/static/js/runtime-main.cbe038c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e84f635ce5fb6ae43d68abe606324b53.js"
+  "/precache-manifest.fa58e0430b46885aed82fb33c2959b18.js"
 );
 
 self.addEventListener('message', (event) => {
